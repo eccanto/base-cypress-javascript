538b3e72226fdc49692a2f64c2f5a15396da09fc
Erik Ccanto <ccanto.erik@gmail.com>
2022-09-19 21:15:42 -0500

    refactor: refactor BDD

--- cypress/e2e/scenarios/transactions.feature
+++ cypress/e2e/scenarios/transactions.feature
@@ -1,4 +1,22 @@
-Feature: Apply filters to transactions
+Feature: Transaction filtering feature
 
-  Scenario: Apply filters to transactions
-    Given Applies the currency filter
+  @suitability
+  Scenario Outline: Verify transaction currency filters
+    Given go to Demo Casino web page
+    And close the welcome pop-up modal
+    And click on the login button
+    And enter the login account
+    When apply the "<currency>" currency filter
+    Then all transactions in the table use currency "<currency>"
+
+    Examples:
+      | currency |
+      | USD      |
+      | EUR      |
+      | ILS      |
+      | mLTC     |
+      | mDOGE    |
+      | mBCH     |
+      | mETH     |
+      | RUB      |
+      | BYR      |
--- cypress/support/step_definitions/registration.js
+++ cypress/support/step_definitions/registration.js
@@ -16,66 +16,48 @@ Given('click on the registration button', function () {
 });
 
 
-When('enter the registration data', function () {
+When('enter the registration data', function (table) {
+    const table_map = table.hashes()[0];
     const inbox_id = this.mail_client.inbox.id.replaceAll('-', '');
 
-    const data = {
-        emailAddress: this.mail_client.inbox.emailAddress,
-        currency: 'EUR',
-        password: 'Password1',
-        question: 'The street your grew up on',
-        answer: 'test',
-        username: inbox_id,
-        bonus: 'No bonus',
-        address: 'address',
-        country: 'Angola',
-        city: 'city',
-        postcode: 'postcode',
-        name: 'name',
-        surname: 'surname',
-        middle_name: 'middle_name',
-        sex: 'Male',
-        nickname: inbox_id,
-    };
-
-    cy.get('[data-test="input-email"]').type(data.emailAddress);
+    cy.get('[data-test="input-email"]').type(this.mail_client.inbox.emailAddress);
 
     cy.get('[data-test="input-terms_and_conditions"]').parent().click();
 
     cy.get('[data-test="input-currency"]').parent().parent().click()
-        .find('.selectric-scroll').contains(data.currency).click();
+        .find('.selectric-scroll').contains(table_map.currency).click();
 
-    cy.get('[data-test="input-password"').type(data.password);
-    cy.get('[data-test="input-password_confirmation"').type(data.password);
+    cy.get('[data-test="input-password"').type(table_map.password);
+    cy.get('[data-test="input-password_confirmation"').type(table_map.password);
 
     cy.get('[data-test="input-secret_question"]').parent().parent().click()
-        .find('.selectric-scroll').contains(data.question).click();
+        .find('.selectric-scroll').contains(table_map.question).click();
 
-    cy.get('[data-test="input-secret_answer"').type(data.answer);
+    cy.get('[data-test="input-secret_answer"').type(table_map.answer);
 
-    cy.get('[data-test="input-username"').type(data.username);
+    cy.get('[data-test="input-username"').type(inbox_id);
 
-    cy.get('#bonus-list-container').contains(data.bonus).click();
+    cy.get('#bonus-list-container').contains(table_map.bonus).click();
 
-    cy.get('[data-test="input-address"').type(data.address);
+    cy.get('[data-test="input-address"').type(table_map.address);
 
     cy.get('[data-test="input-country"]').parent().parent().click()
-        .find('.selectric-scroll').contains(data.country).click();
+        .find('.selectric-scroll').contains(table_map.country).click();
 
-    cy.get('[data-test="input-city"').type(data.city);
+    cy.get('[data-test="input-city"').type(table_map.city);
 
-    cy.get('[data-test="input-postcode"').type(data.postcode);
+    cy.get('[data-test="input-postcode"').type(table_map.postcode);
 
-    cy.get('[data-test="input-name"').type(data.name);
+    cy.get('[data-test="input-name"').type(table_map.name);
 
-    cy.get('[data-test="input-surname"').type(data.surname);
+    cy.get('[data-test="input-surname"').type(table_map.surname);
 
-    cy.get('[data-test="input-middle_name"').type(data.middle_name);
+    cy.get('[data-test="input-middle_name"').type(table_map.middle_name);
 
     cy.get('[data-test="input-sex"]').parent().parent().click()
-        .find('.selectric-scroll').contains(data.sex).click();
+        .find('.selectric-scroll').contains(table_map.sex).click();
 
-    cy.get('[data-test="input-nickname"]').type(data.nickname);
+    cy.get('[data-test="input-nickname"]').type(inbox_id);
 });
 
 
